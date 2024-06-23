5035b5f72134c80e0d42c7110035a55861fe5e70
Srikanth Arrabolu <srikantharrabolu@Srikanths-MacBook-Pro.local>
2024-06-23 17:15:54 -0500

    testing

--- src/router/index.js
+++ src/router/index.js
@@ -12,11 +12,6 @@ const routes = [
     path: '/post/:slug',
     name: 'BlogPostDetails',
     component: BlogPostDetails,
-    meta: {
-      title: 'Default Title',
-      description: 'Default Content',
-      image: 'Default Image URL'
-    },
     props: true
   }
 ];
@@ -26,44 +21,5 @@ const router = createRouter({
   routes
 });
 
-router.beforeEach(async (to, from, next) => {
-  if (to.name === 'BlogPostDetails' && to.params.slug) {
-    const dynamicMeta = await fetchDataBasedOnSlug(to.params.slug);
-    if (dynamicMeta) {
-      to.meta.title = dynamicMeta.title;
-      to.meta.description = dynamicMeta.description;
-      to.meta.image = dynamicMeta.image;
-    }
-  }
-  next();
-});
-
-async function fetchDataBasedOnSlug(slug) {
-  try {
-    const response = await fetch('/data/blogs.json');
-    const data = await response.json();
-    const post = data.find(post => post.slug === slug);
-    if (post) {
-      return {
-        title: post.title,
-        description: post.content,
-        image: `https://northpostglobal.com/images/${post.mainImage}`
-      };
-    } else {
-      return {
-        title: 'Default Title',
-        description: 'Default content',
-        image: 'Default Image URL'
-      };
-    }
-  } catch (error) {
-    console.error('Failed to fetch or process data:', error);
-    return {
-      title: 'Default Title',
-      description: 'Default content',
-      image: 'Default Image URL'
-    };
-  }
-}
 
 export default router;
